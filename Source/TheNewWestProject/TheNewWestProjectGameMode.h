f5e1713c942c8b6e987eece0851c289f9d61152c
Rafal Swierczek <34179rs@gmail.com>
2023-06-28 16:19:15 +0100

    Created New UE Project using First Person Template

---
+++ Source/TheNewWestProject/TheNewWestProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheNewWestProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATheNewWestProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATheNewWestProjectGameMode();
+};
+
+
+
